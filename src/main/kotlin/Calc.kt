cac7abce108cfef418fa7be7f1686a8ed7babdc7
Tony Branch <tonybranch.org@yahoo.com>
2020-12-09 16:44:50 +0300

    Fibonacci, fold().

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -1,3 +1,39 @@
+private fun Int.prin() {
+    println(this)
+}
+
+fun fibo(n: Int): Int {
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        return fibo(n - 1) + fibo(n - 2)
+    }
+
+}
+
+
+fun main() {
+    // Fibonacci 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, …
+    fibo(7).prin()
+
+    val list = listOf(1,2,3,4,5)
+    println(list.fold(15) { total, next -> total + next})
+}
+
+
+/*fun fib(n: Int, a: Int = 0, b: Int = 1): String {
+
+    when (n) {
+        0 -> a
+        1 -> b
+        else -> fib(n - 1, b, a + b)
+    }
+
+    println(n)
+    return ""
+}*/
+
+/*
 fun Any.print(): String = when (this) {
     is String -> "Striiiing!"
     is Int -> "Int"
@@ -8,4 +44,4 @@ fun main() {
     println("asd".print())
     println(1.print())
     println('1'.print())
-}
+}*/
