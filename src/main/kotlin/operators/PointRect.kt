32113d74ae53c7dfc856237fb7e2bbb3e354ad58
Tony Branch <tonybranch.org@yahoo.com>
2020-12-13 19:32:45 +0300

    operator fun Hedgehog.plus(s: Snake)

--- src/main/kotlin/operators/PointRect.kt
+++ src/main/kotlin/operators/PointRect.kt
@@ -13,6 +13,17 @@ operator fun Rect.contains(p: Point) : Boolean {
     return p.x in upperLeft.x .. lowerRight.x && p.y in upperLeft.y .. lowerRight.y
 }
 
+// Скрестить ежа с ужом. Hedgehog.plus(Snake)
+// Hedgehog whoAmI
+// Snake whoAmI
+// BarbWire whoAmI
+
+class Hedgehog { fun whoAmI() = println("I'm a Hedgehog") }
+class Snake { fun whoAmI() = println("I'm a Snake") }
+class BarbWire { fun whoAmI() = println("I'm a BarbWire") }
+
+operator fun Hedgehog.plus(s: Snake) : BarbWire = BarbWire()
+
 fun main() {
     val point1 = Point(3, 3)
     val point2 = Point(5, 5)
@@ -21,4 +32,8 @@ fun main() {
     val rect = Rect(Point(4, 4), Point(20, 10))
     println(point1 in rect)
     println(point2 in rect)
+
+    val hedgehog = Hedgehog()
+    val snake = Snake()
+    (hedgehog + snake).whoAmI()
 }
