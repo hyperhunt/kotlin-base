f175321f9824aa7c1115c8be09f148120cfc6a4d
Tony Branch <tonybranch.org@yahoo.com>
2020-12-13 18:00:59 +0300

    companion object

--- src/main/kotlin/object/Object.kt
+++ src/main/kotlin/object/Object.kt
@@ -1,7 +1,9 @@
 package `object`
 
 // Реализация object
-// 1 - для шаблона singleton
+// 1 - для реализации шаблона singleton
+// 2 - статика в классе
+// 3- создание анонимных внутренних классов
 
 object Singleton {
     init {
@@ -12,7 +14,22 @@ object Singleton {
     fun hello() = println("Object Singleton")
 }
 
+class Outer//    private val data: String = "Harper"
+private constructor() {
+    companion object Hello {
+        fun hello() = println("Outer companion object")
+        fun get(): Outer = Outer()
+    }
+    companion object Loader {
+        fun  hello() = println("Hello from Loader")
+    }
+}
+
 fun main() {
     println(Singleton.name)
     Singleton.hello()
+
+//    val outer = Outer.hello()
+    val outer = Outer.get()
+    Outer.hello()
 }
