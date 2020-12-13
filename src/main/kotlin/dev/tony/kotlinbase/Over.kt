e766fbd74676a2be53001a56597e204426624b0e
Tony Branch <tonybranch.org@yahoo.com>
2020-12-13 14:49:50 +0300

    class -> class, inner, init, internal.

--- src/main/kotlin/dev/tony/kotlinbase/Over.kt
+++ src/main/kotlin/dev/tony/kotlinbase/Over.kt
@@ -31,11 +31,42 @@ class Animator : Animated() {
 }
 
 class AnimatedButton : RichButton() {
-//    override fun click() {} // final
+    //    override fun click() {} // final
     override fun disable() {}
+}
+
+class Outer { // Внешний
+    class Nested {} // Вложенный. Не содержит ссылку на Внешний класс.
+    inner class Internal {
+        fun getOuter(): Outer = this@Outer // Вернуть ссылку на Внешний класс.
+    } // Внутренний.
+}
+
+open class Base {
+    init {
+        println("init Base")
+    }
+}
+
+data class User(val name: String = "Empty") : Base() {
+    init {
+        println("init block...")
+    }
+
+    constructor(name: String, data: Int) : this(name) {
+        println("With data...: $name $data")
+    }
 }
 
 fun main() {
     val face = AnimatedButton()
     face.delete()
+
+    val outer = Outer()
+    val nested = Outer.Nested()
+    val internal = Outer().Internal() // outer.Internal()
+
+//    val user = User()
+    val user = User("Tony", 42)
+    println(user.toString())
 }
