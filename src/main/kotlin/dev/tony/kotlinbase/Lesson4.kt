94146aa7e528210692d359696b101067abaf3144
Tony Branch <tonybranch.org@yahoo.com>
2020-12-13 17:26:46 +0300

    Pair infix fun -> X tot Y.

--- src/main/kotlin/dev/tony/kotlinbase/Lesson4.kt
+++ src/main/kotlin/dev/tony/kotlinbase/Lesson4.kt
@@ -10,14 +10,29 @@ package dev.tony.kotlinbase
     }
 }*/
 
-fun String?.isEmptyOrNull() : Boolean = this?.isEmpty() ?: true
-
+//fun String?.isEmptyOrNull() : Boolean = this?.isEmpty() ?: true
 // "" true
 // null true
 // "hello" false
 
+// Int.r() -> RationalNumber
+// Pair<Int, Int>.r() -> RationalNumber
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
+
+// 34 sm 13 = 47
+// 34.sm(13)
+
+infix fun Int.sm(other: Int) : Int = this + other
+
 fun main() {
-    println("".isEmptyOrNull())
+    println(42.r())
+    println((25 to 45).r())
+    println("${34.sm(13)}")
+    println("${34 sm 23}")
+/*    println("".isEmptyOrNull())
     println(null.isEmptyOrNull())
-    println("hello".isEmptyOrNull())
+    println("hello".isEmptyOrNull())*/
 }
