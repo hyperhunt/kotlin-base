c3ce453d82f2e0ae9d23d97cb8b21e21e5a724e4
Tony Branch <tonybranch.org@yahoo.com>
2020-12-14 16:30:51 +0300

    filter, map, mapValues, count, contains.

--- src/main/kotlin/lambda/Lambda.kt
+++ src/main/kotlin/lambda/Lambda.kt
@@ -5,14 +5,15 @@ data class Person(val name: String, val age: Int)
 val people = listOf(
     Person("Tony", 42),
     Person("Harper", 18),
-    Person("Moskvin", 13),
     Person("Mock", 13),
+    Person("Moskvin", 13),
 )
 
 val list = listOf(1, 2, 3, 4, 5, 6, 7)
 val nullableList: List<Int?> = listOf(1, 2, 3, null, 6, 8)
 val nullableList2: List<Int?>? = listOf(1, 2, 3, null, 6, 8)
 val listDistinct = listOf('a', 'A','b','B','A','B','c')
+val mapData = mapOf(1 to "one", 2 to "two", 3 to "three")
 
 fun main() {
 
@@ -45,4 +46,21 @@ fun main() {
         result::class::simpleName.get()
     )
     println(sum.invoke(8, 108))*/
+
+    println(people.map { it.name + " " + it.age })
+    println(people.filter { it.age >= 18 }.map { it.name })
+    println(list.map { it * it }.filter { it % 3 == 0 })
+
+//    println(mapData.mapValues { it.value.toUpperCase() })
+    mapData.mapValues { it.value.toUpperCase() }.forEach { println(it) }
+
+    // any none all
+    println(people.any { it.age == 13 }) // true критерий соответствия Boolean
+
+    // count countBy
+
+    println("Count: " + people.count())
+    println(people.count { it.age == 13 })
+    println(people.find { it.age == 13 && it.name.contains('k') })
+
 }
