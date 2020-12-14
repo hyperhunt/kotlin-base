ca1736b8a93b131f57c01333a3cfb07beddf1c94
Tony Branch <tonybranch.org@yahoo.com>
2020-12-14 11:10:00 +0300

    ::lambda - вызов по ссылке run

--- src/main/kotlin/view/UI.kt
+++ src/main/kotlin/view/UI.kt
@@ -3,7 +3,45 @@ package view
 import JavaClickListener
 import JavaView
 
+data class Person(val name: String, val age: Int)
+
+val people = listOf(
+    Person("Tony", 42),
+    Person("Harper", 30),
+    Person("Moskvin", 13),
+)
+
+fun hi() = println("hi")
+
+fun Person.isAdult() : Boolean = age >= 18
+
 fun main() {
+
+//    println(people.filter { it -> it.age < 30 })
+    println(people.minByOrNull { x: Person -> x.age })
+    println(people.minOf { x: Person -> x.age })
+
+    var counter = 0
+    people.forEach { _ -> counter++ }
+    println("counter: $counter")
+
+    val create = ::Person
+    println(create("Tony", 42))
+
+    val adult = Person::isAdult
+    println(
+        people.filter(adult)
+    )
+
+    val harpName = create("Harp", 11)
+    val harpAge = harpName::age
+    println("harpAge: ${harpAge.get()}")
+
+    val personAge = Person::age // Ссылка на метод класс
+    val hello = ::hi // Ссылка на функцию
+    run(hello) // Вызов функции по ссылке
+
+
     val javaView = JavaView()
     javaView.setOnClickListener(object: JavaClickListener {
         override fun onClick(view: JavaView?) {
@@ -13,7 +51,7 @@ fun main() {
 
     javaView.setOnClickListener { view -> println("javaView -> lambda") }
 
-    val sum = { x: Int, y: Int -> x + y}
+    val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y}
     println(sum(5, 8))
 
     /*
