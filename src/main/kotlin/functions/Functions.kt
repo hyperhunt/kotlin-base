5698245c6a4a96d7bded7b78150adcc4475ad981
Tony Branch <tonybranch.org@yahoo.com>
2020-12-15 13:20:12 +0300

    Расширение типа Int, Int.().

--- src/main/kotlin/functions/Functions.kt
+++ src/main/kotlin/functions/Functions.kt
@@ -1,5 +1,14 @@
 package functions
 
+// Функции расширения Int
+// iOdd isEven()
+//fun Int.isOdd(): Boolean = this % 2 != 0
+//fun Int.isEven(): Boolean = this % 2 == 0
+
+// Сделать как свойство расширения
+val isOdd: Int.() -> Boolean = { this % 2 != 0 }
+val isEven: Int.() -> Boolean = { this % 2 == 0 }
+
 class LazyClass(val initializer: () -> Int) {
     val initial: Int by lazy { initializer() }
     // 1. initial == initializer()
@@ -27,4 +36,10 @@ fun main() {
     lazyClass.initial
     lazyClass.initial
     lazyClass.initial
+
+//    println(2.isOdd())
+//    println(2.isEven())
+
+//    println((2).isOdd())
+//    println((2).isEven())
 }
