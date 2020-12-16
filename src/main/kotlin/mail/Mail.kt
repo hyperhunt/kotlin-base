c09f0e910314b77b0db908f0611e6574fc926d6f
Tony Branch <tonybranch.org@yahoo.com>
2020-12-16 10:59:56 +0300

    Mailer

---
+++ src/main/kotlin/mail/Mail.kt
@@ -0,0 +1,15 @@
+package mail
+
+class PersonalInfo(val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    if(client?.personalInfo?.email != null && message != null) mailer.sendMessage(client.personalInfo.email, message)
+}
+
+fun main() {
+}
