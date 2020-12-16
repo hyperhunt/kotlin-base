706d748d69a48b5b893bb8c700d9f0f3652da28c
Tony Branch <tonybranch.org@yahoo.com>
2020-12-16 15:04:20 +0300

    @ParameterizedTest, @CsvSource.

--- src/test/kotlin/calculator/CalculatorTest.kt
+++ src/test/kotlin/calculator/CalculatorTest.kt
@@ -1,17 +1,36 @@
 package calculator
 
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.provider.CsvSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // Использовать один класс для всех тестов
+//@DisplayName("Calculator Add test func")
 class CalculatorTest {
     init {
         println("Init...")
     }
 
+    @BeforeAll
+    fun prepare() {
+        println("prepare() BeforeAll")
+    }
+
+    @AfterAll
+    fun tearDown() {
+        println("tearDown() AfterAll")
+    }
+
+    @BeforeEach
+    fun prepareTest() {
+        println("prepareTest() BeforeEach")
+    }
+
+    //    @DisplayName("Calculator Add test func")
     @Test
-    fun calcAdd() {
+    fun `Calculator Method Add`() {
         println("calcAdd")
         val calculator = Calculator(15)
         calculator.add(2)
@@ -22,6 +41,7 @@ class CalculatorTest {
         )
     }
 
+    //    @DisplayName("\uD83D\uDC4D")
     @Test
     fun calcMul() {
         println("calcMul")
@@ -31,6 +51,23 @@ class CalculatorTest {
             50,
             calculator.get(),
             "calcMul: [25 * 2 should give 50]"
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0,1,1",
+        "10,5,15",
+        "1,100,101",
+    )
+    fun `pairAdd with @CsvSource`(f: Int, s: Int, r: Int) {
+        println("pairAdd with @CsvSource")
+        val calculator = Calculator(f)
+        calculator.add(s)
+        assertEquals(
+            r,
+            calculator.get(),
+            "pairAdd: [$f + $s should give $r]"
         )
     }
 }
