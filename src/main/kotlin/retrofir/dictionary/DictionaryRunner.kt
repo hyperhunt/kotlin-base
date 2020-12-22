840cd4d9bdf9b946f16453717ee92a81bb3a3d18
Tony Branch <tonybranch.org@yahoo.com>
2020-12-22 21:07:36 +0300

    Retrofi2: yandexDictionaryService example.

---
+++ src/main/kotlin/retrofir/dictionary/DictionaryRunner.kt
@@ -0,0 +1,22 @@
+package retrofir.dictionary
+
+import retrofit2.Retrofit
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import retrofit2.create
+
+val retrofit: Retrofit = Retrofit.Builder()
+    .baseUrl("https://dictionary.yandex.net")
+    .addConverterFactory(SimpleXmlConverterFactory.create())
+    .build()
+
+val yandexDictionaryService = retrofit.create(YandexDictionaryService::class.java)
+
+fun main() {
+    val response = yandexDictionaryService.lookup("en-ru", "fighter jet").execute() // Синхронное выполнение запроса.
+    if(response.isSuccessful) {
+        val result = response.body()
+        println(
+            result?.defs?.get(0)?.trs?.get(0)?.text
+        )
+    }
+}
