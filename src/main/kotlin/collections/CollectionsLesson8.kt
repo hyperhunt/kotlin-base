1e9d2fd26112119a66485a8737ea1043fdfde1eb
Tony Branch <tonybranch.org@yahoo.com>
2020-12-23 10:18:54 +0300

    Collections: List, Set, Map.

---
+++ src/main/kotlin/collections/CollectionsLesson8.kt
@@ -0,0 +1,16 @@
+package collections// Коллекции List Set Map
+
+fun main() {
+/*    val list = listOf(1, 2, 3, 4)
+    val list2 = listOf(1, 2, 3, 4)
+    println(list == list2)
+//    list[2] = 12
+    val mutableList = mutableListOf(1, 2, 3, 4)
+    mutableList[2] = 12
+    println(mutableList)
+
+    val set = setOf("one", "two", "three") // LinkedHashSet HashSet SortedSet
+    setOf(set)
+
+    val map = mapOf(1 to "one", 2 to "two") // LinkedHashMap HashMap SortedMap*/
+}
