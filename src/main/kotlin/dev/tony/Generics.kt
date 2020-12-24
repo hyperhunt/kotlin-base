ba9445174ae86f0c21987fc66f5f09c3712f9636
Tony Branch <tonybranch.org@yahoo.com>
2020-12-24 16:34:46 +0300

    Generics.kt: class Something<T>(p: T)

--- src/main/kotlin/dev/tony/Generics.kt
+++ src/main/kotlin/dev/tony/Generics.kt
@@ -6,6 +6,17 @@ package dev.tony
 }*/
 fun <T> returnTwoElements(obj: T): List<T> = listOf(obj, obj)
 
+fun <T> parePrint(obj: T, ch: Char) = when (ch) {
+    '{', '}' -> println("{$obj}")
+    '[', ']' -> println("[$obj]")
+    '(', ')' -> println("($obj)")
+    else -> println("Unknown parse symbol: '$ch'.")
+}
+
+class Something<T>(p: T) {
+    val prop: T = p
+}
+
 fun main() {
     val number = 42
     val str = "str"
@@ -14,4 +25,15 @@ fun main() {
     println(returnTwoElements(number))
     println(returnTwoElements(str))
     println(returnTwoElements(doubleNumber))
+
+    parePrint(number, '{')
+    parePrint(str, '[')
+    parePrint(doubleNumber, '(')
+    parePrint(doubleNumber, '=')
+
+    println("\nClass<T>")
+    val aSomething: Something<Int> = Something(42)
+    val sSomething: Something<String> = Something("String")
+    println(aSomething.prop)
+    println(sSomething.prop)
 }
