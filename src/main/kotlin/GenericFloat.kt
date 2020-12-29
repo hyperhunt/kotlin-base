ce6f8d6050a65210f008f7dbe289d8865bf43021
Tony Branch <tonybranch.org@yahoo.com>
2020-12-29 12:26:41 +0300

    Add example GenericFloat.kt: Sequence

---
+++ src/main/kotlin/GenericFloat.kt
@@ -0,0 +1,14 @@
+import kotlin.random.Random
+
+fun genericDouble() {
+    val numberGeneric: Sequence<String> =
+        generateSequence { Random.nextDouble(0.0, 1.0).toString().take(3) }
+    numberGeneric.take(10).forEach { println(it) }
+}
+
+fun main() {
+    genericDouble()
+    println(List(10) { Random.nextFloat() })
+//    val randomValues = List(10) { Random.nextDouble(0.0, 1.0).toString().take(3) }
+//    println(randomValues)
+}
