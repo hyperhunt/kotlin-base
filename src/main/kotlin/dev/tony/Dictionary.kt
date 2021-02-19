38b84f5331b7421f474d44ef5d57534dad783bd5
Tony Branch <tonybranch.org@yahoo.com>
2021-02-19 19:22:10 +0300

    Directory "dict" init.

---
+++ src/main/kotlin/dev/tony/Dictionary.kt
@@ -0,0 +1,9 @@
+package dev.tony
+
+// API key
+// dict.1.1.20170402T202525Z.1ed8dd7c23205718.efda3a2c4bdf834b16909622280b20b0d60840a1
+
+/*
+https://dictionary.yandex.net/api/v1/dicservice/lookup?key=dict.1.1.20170402T202525Z.1ed8dd7c23205718.efda3a2c4bdf834b16909622280b20b0d60840a1&lang=en-ru&text=time
+*/
+
