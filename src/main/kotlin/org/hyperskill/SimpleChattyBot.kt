253f52f226f87720c7996da29e936621651342b0
Tony Branch <tonybranch.org@yahoo.com>
2021-02-22 12:16:52 +0300

    Hyperskill: Modulo division.

---
+++ src/main/kotlin/org/hyperskill/SimpleChattyBot.kt
@@ -0,0 +1,18 @@
+package org.hyperskill
+
+fun main() {
+    val x = 0
+
+    println((x % 3).toString() + " = 0")
+}
+
+/*
+class Person {
+    val name: String = "Vlad"
+
+    fun <T> print() {
+        println(this@Person.name)
+    }
+}
+
+fun main() = Person().print<String>()*/
