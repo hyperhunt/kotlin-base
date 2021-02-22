949d618ec6f45dbace634e3efffe1dd0ddd14fdd
Tony Branch <tonybranch.org@yahoo.com>
2021-02-22 13:32:27 +0300

    Hyperskill: Modulo division completed.

--- src/main/kotlin/org/hyperskill/SimpleChattyBot.kt
+++ src/main/kotlin/org/hyperskill/SimpleChattyBot.kt
@@ -1,10 +1,59 @@
 package org.hyperskill
 
+
+
+/*fun main() {
+    val x = 25 % 12
+    println("x = $x")
+
+    val y = 5
+    println("y = ${8 % y}")
+    println(13 % 6)
+
+//    println(x = n⋅y + z)
+
+}*/
+
+/*
+
 fun main() {
-    val x = 0
+    */
+/*val x = 0
 
     println((x % 3).toString() + " = 0")
+    println((x % 2).toString() + " = 0")
+    println((x % 2).toString() + " = 1")
+    println((x % 4).toString() + " = 1")*//*
+
+
+    val mod = listOf(
+        Pair(3, 0),
+        Pair(2, 0),
+        Pair(2, 1),
+        Pair(4, 1)
+    )
+    mod.forEach {  }
+
+    var x = 0
+    var modCheck: Boolean = false
+
+    while (!modCheck) {
+        mod.forEach { it: Pair<Int, Int> ->
+            modCheck = x % it.first != it.second
+            println(x.toString() + " mod " + it.first.toString() + " = " + it.second.toString() + " " + modCheck)
+
+            if (modCheck) {
+                modCheck = false
+                return@forEach
+            }
+
+        }
+
+        x++
+        println(" ")
+    }
 }
+*/
 
 /*
 class Person {
