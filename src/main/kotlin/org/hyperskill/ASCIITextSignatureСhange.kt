ec2a21d1f20d2412d006183a333e8a9ebd2529c6
Tony Branch <tonybranch.org@yahoo.com>
2021-02-25 22:11:41 +0300

    Hyperskill: Arrays completed.

--- src/main/kotlin/org/hyperskill/ASCIITextSignatureСhange.kt
+++ src/main/kotlin/org/hyperskill/ASCIITextSignatureСhange.kt
@@ -1,5 +1,56 @@
 package org.hyperskill
 
+//
+// Data types and variables
+//
+
+//You completed Arrays
+
+/*fun main() {
+//    val longs = longArrayOf(100_000_000_001, 100_000_000_002, 100_000_000_003)
+//    println(longs.joinToString())
+
+//    val numbers = intArrayOf(12, 17, 8, 101, 33)
+
+    val numbers = IntArray(100)
+    numbers[0] = 1
+    numbers[9] = 10
+    numbers[99] = 100
+
+    numbers.forEach { print( "$it ") }
+
+}*/
+
+/*object p1 {
+    val c1 = "c"
+}
+
+object p2 {
+    val c1= "c"
+}
+
+object p3 {
+    val asd = 123
+}
+
+fun main() {
+    println("p1 == p2 --> ${p1.c1 == p2.c1} ")
+}*/
+
+/*interface SpaceColor {
+    var color: String?
+}
+
+calendar
+
+open class Engine(override var color: String? = null) : SpaceColor
+data class SpaceShip(override var color: String? = "White") : Engine(color)
+
+fun main() {
+    val spaceship = SpaceShip().
+    println(spaceship.color)
+}*/
+
 /* Print star.
 fun main() {
 
