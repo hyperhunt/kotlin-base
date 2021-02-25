cab40318d706c11ab1e91d2a3df715dc1d2a5d72
Tony Branch <tonybranch.org@yahoo.com>
2021-02-25 14:22:35 +0300

    Hyperskill: ASCII Text () completed.

---
+++ src/main/kotlin/org/hyperskill/ASCIITextSignatureСhange.kt
@@ -0,0 +1,27 @@
+package org.hyperskill
+
+/* "Hyper Skill"
+
+fun main() {
+    val charLineVertical = '|'
+    val charLineHorizontal = '_'
+    val charDote = '•'
+
+    val text = "Hyper Skill"
+
+    print(" ")
+    repeat(text.length + 2) {
+        print(charLineHorizontal)
+    }
+    println("")
+
+    print(charLineVertical)
+    print(" $text ")
+    println(charLineVertical)
+
+    print(" ")
+    repeat(text.length + 2) {
+        print(charDote)
+    }
+    println("")
+}*/
