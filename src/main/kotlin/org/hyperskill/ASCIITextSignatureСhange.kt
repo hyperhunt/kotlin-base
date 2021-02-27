8592796f70b97f11751d673b4e86f7e064af4909
Tony Branch <tonybranch.org@yahoo.com>
2021-02-27 12:02:55 +0300

    Hyperskill: FontASCII add Enum.

--- src/main/kotlin/org/hyperskill/ASCIITextSignatureСhange.kt
+++ src/main/kotlin/org/hyperskill/ASCIITextSignatureСhange.kt
@@ -1,8 +1,55 @@
 package org.hyperskill
 
 //
+// FontASCII
+//
+
+enum class FontASCII(val ch: List<String>) {
+
+    A(listOf(
+        """
+        ____
+    """.trimIndent(),
+        """
+        |__|
+    """.trimIndent(),
+        """
+        |  |
+    """.trimIndent()
+    ));
+
+
+}
+
+fun main() {
+
+
+
+
+}
+
+
+//
 // Data types and variables
 //
+
+//    val dictionary = mutableMapOf<Char, listOf>()
+//    dictionary['a'] = font.a
+
+//    operator fun get(i: Int): Any? {
+//        return a.ch
+//    }
+//    val a = listOf(
+//        """
+//        ____
+//    """.trimIndent(),
+//        """
+//        |__|
+//    """.trimIndent(),
+//        """
+//        |  |
+//    """.trimIndent()
+//    )
 
 /*object fontASCII {
     val a = """
@@ -13,214 +60,236 @@ package org.hyperskill
 
 }*/
 
-class FontASCII {
-    val a = """
-        ____
-        |__|
-        |  |
-    """.trimIndent()
-
-    val b = """
-        ___ 
-        |__]
-        |__]
-    """.trimIndent()
-
-    val c = """
-        ____
-        |   
-        |___
-    """.trimIndent()
-
-    val d = """
-        ___ 
-        |  \
-        |__/
-    """.trimIndent()
-
-    val e = """
-        ____
-        |___
-        |___
-    """.trimIndent()
-
-    val f = """
-        ____
-        |___
-        |   
-    """.trimIndent()
-
-    val g = """
-        ____
-        | __
-        |__]
-    """.trimIndent()
-
-    val h = """
-        _  _
-        |__|
-        |  |
-    """.trimIndent()
-
-    val i = """
-        _
-        |
-        |
-    """.trimIndent()
-
-    val j = """
-         _
-         |
-        _|
-    """.trimIndent()
-
-    val k = """
-        _  _
-        |_/ 
-        | \_
-    """.trimIndent()
-
-    val l = """
-        _   
-        |   
-        |___
-    """.trimIndent()
-
-    val m = """
-        _  _
-        |\/|
-        |  |
-    """.trimIndent()
-
-    val n = """
-        _  _
-        |\ |
-        | \|
-    """.trimIndent()
-
-    val o = """
-        ____
-        |  |
-        |__|
-    """.trimIndent()
-
-    val p = """
-        ___ 
-        |__]
-        |   
-    """.trimIndent()
-
-    val q = """
-        ____
-        |  |
-        |_\|
-    """.trimIndent()
-
-    val r = """
-        ____
-        |__/
-        |  \
-    """.trimIndent()
-
-    val s = """
-        ____
-        [__ 
-        ___]
-    """.trimIndent()
-
-    val t = """
-        ___
-         | 
-         | 
-    """.trimIndent()
-
-    val u = """
-        _  _
-        |  |
-        |__|
-    """.trimIndent()
-
-    val v = """
-        _  _
-        |  |
-         \/ 
-    """.trimIndent()
-
-    val w = """
-        _ _ _
-        | | |
-        |_|_|
-    """.trimIndent()
-
-    val x = """
-        _  _
-         \/ 
-        _/\_
-    """.trimIndent()
-
-    val y = """
-        _   _
-         \_/ 
-          |  
-    """.trimIndent()
-
-    val z = """
-        ___
-          /
-         /__
-    """.trimIndent()
-
-    val space = """
-        #
-        #
-        #
-    """.trimIndent()
-}
-
-fun main() {
-    for (i in 'a'..'z') {
-        print("$i ")
-    }
-
-    println("")
-
-    val font = FontASCII()
-
-    val dictionary: MutableMap<Char, String> = mutableMapOf()
-    dictionary['a'] = font.a
-    dictionary['b'] = font.b
-    dictionary['c'] = font.c
-    dictionary['d'] = font.d
-    dictionary['e'] = font.e
-    dictionary['f'] = font.f
-    dictionary['g'] = font.g
-    dictionary['h'] = font.h
-    dictionary['i'] = font.i
-    dictionary['j'] = font.j
-    dictionary['k'] = font.k
-    dictionary['l'] = font.l
-    dictionary['m'] = font.m
-    dictionary['n'] = font.n
-    dictionary['o'] = font.o
-    dictionary['p'] = font.p
-    dictionary['q'] = font.q
-    dictionary['r'] = font.r
-    dictionary['s'] = font.s
-    dictionary['t'] = font.t
-    dictionary['u'] = font.u
-    dictionary['v'] = font.v
-    dictionary['w'] = font.w
-    dictionary['x'] = font.x
-    dictionary['y'] = font.y
-    dictionary['z'] = font.z
-
-
-    println(dictionary['a'][0])
-//    for (i in 'a'..'m') print("${dictionary[i]} ${font.space}")
+//
+//class FontASCII {
+//    val a = """
+//        ____
+//        |__|
+//        |  |
+//    """.trimIndent()
+//
+//    val b = """
+//        ___
+//        |__]
+//        |__]
+//    """.trimIndent()
+//
+//    val c = """
+//        ____
+//        |
+//        |___
+//    """.trimIndent()
+//
+//    val d = """
+//        ___
+//        |  \
+//        |__/
+//    """.trimIndent()
+//
+//    val e = """
+//        ____
+//        |___
+//        |___
+//    """.trimIndent()
+//
+//    val f = """
+//        ____
+//        |___
+//        |
+//    """.trimIndent()
+//
+//    val g = """
+//        ____
+//        | __
+//        |__]
+//    """.trimIndent()
+//
+//    val h = """
+//        _  _
+//        |__|
+//        |  |
+//    """.trimIndent()
+//
+//    val i = """
+//        _
+//        |
+//        |
+//    """.trimIndent()
+//
+//    val j = """
+//         _
+//         |
+//        _|
+//    """.trimIndent()
+//
+//    val k = """
+//        _  _
+//        |_/
+//        | \_
+//    """.trimIndent()
+//
+//    val l = """
+//        _
+//        |
+//        |___
+//    """.trimIndent()
+//
+//    val m = """
+//        _  _
+//        |\/|
+//        |  |
+//    """.trimIndent()
+//
+//    val n = """
+//        _  _
+//        |\ |
+//        | \|
+//    """.trimIndent()
+//
+//    val o = """
+//        ____
+//        |  |
+//        |__|
+//    """.trimIndent()
+//
+//    val p = """
+//        ___
+//        |__]
+//        |
+//    """.trimIndent()
+//
+//    val q = """
+//        ____
+//        |  |
+//        |_\|
+//    """.trimIndent()
+//
+//    val r = """
+//        ____
+//        |__/
+//        |  \
+//    """.trimIndent()
+//
+//    val s = """
+//        ____
+//        [__
+//        ___]
+//    """.trimIndent()
+//
+//    val t = """
+//        ___
+//         |
+//         |
+//    """.trimIndent()
+//
+//    val u = """
+//        _  _
+//        |  |
+//        |__|
+//    """.trimIndent()
+//
+//    val v = """
+//        _  _
+//        |  |
+//         \/
+//    """.trimIndent()
+//
+//    val w = """
+//        _ _ _
+//        | | |
+//        |_|_|
+//    """.trimIndent()
+//
+//    val x = """
+//        _  _
+//         \/
+//        _/\_
+//    """.trimIndent()
+//
+//    val y = """
+//        _   _
+//         \_/
+//          |
+//    """.trimIndent()
+//
+//    val z = """
+//        ___
+//          /
+//         /__
+//    """.trimIndent()
+//
+//    val space = """
+//        #
+//        #
+//        #
+//    """.trimIndent()
+//}
+//
+//fun main() {
+//    for (i in 'a'..'z') {
+//        print("$i ")
+//    }
+//
 //    println("")
-//    for (i in 'n'..'z') print("${dictionary[i]}")
-//    println("")
-}
+//
+//    val font = FontASCII()
+//
+//    val dictionary: MutableMap<Char, String> = mutableMapOf()
+//    dictionary['a'] = font.a
+//    dictionary['b'] = font.b
+//    dictionary['c'] = font.c
+//    dictionary['d'] = font.d
+//    dictionary['e'] = font.e
+//    dictionary['f'] = font.f
+//    dictionary['g'] = font.g
+//    dictionary['h'] = font.h
+//    dictionary['i'] = font.i
+//    dictionary['j'] = font.j
+//    dictionary['k'] = font.k
+//    dictionary['l'] = font.l
+//    dictionary['m'] = font.m
+//    dictionary['n'] = font.n
+//    dictionary['o'] = font.o
+//    dictionary['p'] = font.p
+//    dictionary['q'] = font.q
+//    dictionary['r'] = font.r
+//    dictionary['s'] = font.s
+//    dictionary['t'] = font.t
+//    dictionary['u'] = font.u
+//    dictionary['v'] = font.v
+//    dictionary['w'] = font.w
+//    dictionary['x'] = font.x
+//    dictionary['y'] = font.y
+//    dictionary['z'] = font.z
+//
+//    val l1 = mutableListOf<Char>('a','b','c')
+//    val l2 = arrayListOf<Char>('a','b','c')
+//    val l3 = arrayOf('a','b','c')
+//
+//    println(l1.add('s'))
+//    println(l1)
+//
+//    l2.add('e')
+//    println(l2)
+//
+//    l3[0] = 'd'
+//    l3.forEach { print(it) }
+//
+//    val l4 = CharArray(3)
+//    l4[0] = 'a'
+//    l4[1] = 'b'
+//    l4[2] = 'c'
+//
+//    print("\n--> ")
+//    l4.forEach { print(it) }
+//
+//
+////    println(dictionary['a'][0])
+////    for (i in 'a'..'m') print("${dictionary[i]} ${font.space}")
+////    println("")
+////    for (i in 'n'..'z') print("${dictionary[i]}")
+////    println("")
+//}
 
 
 //    val ch = arrayListOf<String>(
