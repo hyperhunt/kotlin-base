d6571162084f5b81bd6397f82fdd6aacd53ea4a3
Tony Branch <tonybranch.org@yahoo.com>
2021-02-28 14:00:06 +0300

    Hyperskill: String arrays completed.

--- src/main/kotlin/org/hyperskill/Substring.kt
+++ src/main/kotlin/org/hyperskill/Substring.kt
@@ -4,11 +4,22 @@ fun String.prin() {
     println("--> $this")
 }
 
-fun main() {
-    "content".substring(1,1).prin()
-    "content".substring(0,0).prin()
-//    "content".substring(-1,-1).prin()
-    "content".substringAfter("t").prin()
-    "content".substring(0,1).prin()
-    "content".substringBefore("c").prin()
-}
+// String arrays
+
+//val capitals = arrayOf("Tokyo", "Moscow", "Paris", "Washington", "Beijing")
+//capitals[0] = "Tokyo"
+//capitals[1] = "Moscow"
+//capitals[2] = "Paris"
+//capitals[3] = "Washington"
+//capitals[4] = "Beijing"
+//
+//println(capitals)
+
+//fun main() {
+//    "content".substring(1,1).prin()
+//    "content".substring(0,0).prin()
+////    "content".substring(-1,-1).prin()
+//    "content".substringAfter("t").prin()
+//    "content".substring(0,1).prin()
+//    "content".substringBefore("c").prin()
+//}
